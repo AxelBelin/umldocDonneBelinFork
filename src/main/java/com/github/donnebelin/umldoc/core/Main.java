00beafb560b051592b3bfdaf38bffd797194ba8d
AxelBelin <64922136+AxelBelin@users.noreply.github.com>
2022-10-17 21:10:58 +0200

    JAI VIRÉ LES MAUVAIS FICHIERS QUI ÉTAIENT DES ANCIENNES VERSIONS.

--- src/main/java/com/github/donnebelin/umldoc/core/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.donnebelin.umldoc.core;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        var entities = JarParser.parse();
-    }
-}
