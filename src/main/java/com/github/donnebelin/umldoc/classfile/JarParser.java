1639d4fc3be011c8c417fc4a93fe72f75498f9ec
AxelBelin <axel.belin07@gmail.com>
2022-10-18 09:10:24 +0200

    Tout est ok on peut faire la pull request.

--- src/main/java/com/github/donnebelin/umldoc/classfile/JarParser.java
+++ src/main/java/com/github/donnebelin/umldoc/classfile/JarParser.java
@@ -1,8 +1,8 @@
 package com.github.donnebelin.umldoc.classfile;
 
 import com.github.forax.umldoc.core.AssociationDependency;
-import com.github.forax.umldoc.core.AssociationDependency.Side;
 import com.github.forax.umldoc.core.AssociationDependency.Cardinality;
+import com.github.forax.umldoc.core.AssociationDependency.Side;
 import com.github.forax.umldoc.core.Entity;
 import com.github.forax.umldoc.core.Entity.Stereotype;
 import com.github.forax.umldoc.core.Field;
@@ -11,9 +11,13 @@ import java.io.IOException;
 import java.lang.constant.ClassDesc;
 import java.lang.module.ModuleFinder;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
@@ -42,16 +46,17 @@ public final class JarParser {
 
 
   /**
-   * Supplies a list of all Associations parsed from jar file
+   * Supplies a list of all Associations parsed from jar file.
    *
-   * @return A list of AssociationDependency object with all parsed associations discovered in the jar file.
+   * @return A list of AssociationDependency object
+   *      with all parsed associations discovered in the jar file.
    */
-  public List<AssociationDependency> getAssociationDependencies(){
+  public List<AssociationDependency> getAssociationDependencies() {
     return List.copyOf(associations);
   }
 
   /**
-   * Supplies a list of all Entities parsed from jar file
+   * Supplies a list of all Entities parsed from jar file.
    *
    * @return A list of Entity object with all parsed entities discovered in the jar file.
    */
@@ -75,7 +80,7 @@ public final class JarParser {
                   .open(filename).orElseThrow()) {
             var classReader =
                     new ClassReader(inputStream);
-            getASMData(classReader);
+            getAsmData(classReader);
           }
         }
       }
@@ -83,15 +88,15 @@ public final class JarParser {
   }
 
   private static Stereotype resolveStereotype(String superName) {
-    if(superName.contains("Record")) {
+    if (superName.contains("Record")) {
       return Stereotype.RECORD;
     }
 
-    if(superName.contains("Enum")) {
+    if (superName.contains("Enum")) {
       return Stereotype.ENUM;
     }
 
-    if(superName.contains("Interface")) {
+    if (superName.contains("Interface")) {
       return Stereotype.INTERFACE;
     }
 
@@ -122,7 +127,11 @@ public final class JarParser {
     return modifiers;
   }
 
-  private void addFieldOrAssociation(int access, String name, String type, HashSet<Field> currentFields) {
+  private void addFieldOrAssociation(
+          int access,
+          String name,
+          String type,
+          HashSet<Field> currentFields) {
     var field = new Field(
             modifiers(access),
             name.replace('$', '_'),
@@ -131,24 +140,34 @@ public final class JarParser {
     var isAsso = false;
     int startIndex;
     int endIndex;
-    while((startIndex = type.indexOf("<")) >= 0 && (endIndex = type.lastIndexOf(">")) >= 0){
-      type = type.substring(startIndex + 1,endIndex);
-      if(!type.contains("<")){
+    while ((startIndex = type.indexOf("<")) >= 0 && (endIndex = type.lastIndexOf(">")) >= 0) {
+      type = type.substring(startIndex + 1, endIndex);
+      if (!type.contains("<")) {
         var tempType = type;
         var entityRight = entities.stream()
                 .filter(entity -> entity.name().contains(tempType))
                 .findFirst();
 
-        if(entityRight.isPresent()){
+        if (entityRight.isPresent()) {
           isAsso = true;
-          var left = new Side(currentEntity, Optional.empty(), true, Cardinality.ONLY_ONE);
-          var right = new Side(entityRight.get(), Optional.of(field.name()), true, Cardinality.MANY);
+          var left = new Side(
+                  currentEntity,
+                  Optional.empty(),
+                  true,
+                  Cardinality.ONLY_ONE
+          );
+          var right = new Side(
+                  entityRight.get(),
+                  Optional.of(field.name()),
+                  true,
+                  Cardinality.MANY
+          );
           associations.add(new AssociationDependency(left, right));
         }
       }
     }
 
-    if(!isAsso) {
+    if (!isAsso) {
       currentFields.add(field);
     }
 
@@ -166,7 +185,7 @@ public final class JarParser {
     entities.add(currentEntity);
   }
 
-  private void getASMData(ClassReader classReader) {
+  private void getAsmData(ClassReader classReader) {
     var currentFields = new HashSet<Field>();
     classReader.accept(new ClassVisitor(Opcodes.ASM9) {
 
@@ -190,16 +209,26 @@ public final class JarParser {
       }
 
       @Override
-      public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-        if(! name.contains("this")) {
-          if(signature != null) {
+      public FieldVisitor visitField(
+              int access,
+              String name,
+              String descriptor,
+              String signature,
+              Object value) {
+        if (! name.contains("this")) {
+          if (signature != null) {
             signature = signature.replaceAll(";", "").replaceAll("<", "</");
             addFieldOrAssociation(access, name, Arrays.stream(signature.split("/"))
                     .filter(part -> part.contains("<") || part.contains(">"))
                     .collect(Collectors.joining())
                     .replace('$', '_'), currentFields);
           } else {
-            addFieldOrAssociation(access, name, ClassDesc.ofDescriptor(descriptor).displayName(), currentFields);
+            addFieldOrAssociation(
+                    access,
+                    name,
+                    ClassDesc.ofDescriptor(descriptor).displayName(),
+                    currentFields
+            );
           }
         }
 
